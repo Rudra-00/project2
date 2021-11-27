93dcc0397d9cab02d0288af32e23877dc5164dfa
Rudra-00 <82320714+Rudra-00@users.noreply.github.com>
2021-11-27 13:47:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+ function preload(){
+    
+ }
+ function setup(){
+canvas=createCanvas(400,400);
+canvas.position(550,200);
+video=createCapture(VIDEO);
+video.hide();
+ }
+function  draw(){
+
+}
+
+
